782832249d561fc3e29bbab78a21457719666da7
Sunny Sharma <123087797+sharmaofficiall@users.noreply.github.com>
2024-10-09 21:20:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+API_ID = 28777897  # api id
+API_HASH = "860bea63eeaffe1c439697bc64f5abf3"  # api hash
+
+BOT_TOKEN = "7851351766:AAGXeQUC-Y_EpWM77YpJsLn557nX5cP3sYs"  # bot token
+
+
+# REDIS
+HOST = "redis-14925.c321.us-east-1-2.ec2.redns.redis-cloud.com"  # redis host uri
+PORT = 14925  # redis port
+PASSWORD = "uZvffjSL2SFiGk8CRvwBdPvp1EUDdMqX"  # redis password
+
+PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+# COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
+COOKIE = "csrfToken=Lznbxi0-DCVIFJXBeT6Hle_h; _tt_enable_cookie=1; _ttp=DEKcJOp8G78pNnnaQn7xJJQx8-T; ndut_fmt=75E68F37660FF9A3BCD58E5F5D37A7EAC740AFB8BD1ADD8A2F6828A1CB18C413; ab_sr=1.0.1_ZTIzOGYyYTAxODgwMThiYTU3MDM2N2E4MjBmMTZlNzFjYjczNWIzODFhZjEyYzlkYjEzOTVhMTU5YTdhZjgyMDhjZTkwOWE2YTNjNDIzNGFjZGMwNjk3ZmIwOTQ0ODJlMjY0MGY5NmMwMTI3YWYwYzc1OTcyMjE0NWVhZjhkMDFkYThkNzdkNTkxMjhlMGMwYzI1ZWY1YWU0NzFlMzNmZg==; _ga_RSNVN63CM3=GS1.1.1728486133.1.0.1728486135.58.0.0"
+ADMINS = [5035292765]
+
+
+BOT_USERNAME = "teraaaboxxdownloader_bot"
+
+# Force user to join this channel. (make sure you have promoted the bot on this chat.)
+FORCE_LINK = "@sharmamod01"
+
+# PUBLIC_EARN_API = ""  # https://publicearn.com/api
